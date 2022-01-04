08d9ab9e194ec53feaf503ff274a3dc212a04319
Tom L <devzemux1613@gmail.com>
2022-01-04 16:05:23 +0100

    feat: add isValidDate function

--- datafunctionsImport.sql
+++ datafunctionsImport.sql
@@ -14,3 +14,18 @@ BEGIN
     END IF;
 END$$
 DELIMITER ;
+
+-- This function will return 1 in case that the target data is a valid date. In all other cases the function return 0
+
+DELIMITER $$
+CREATE OR REPLACE FUNCTION isValidDate(vDate DATE)
+returns INT
+BEGIN
+	DECLARE vMonth, vDay INT; 
+    SELECT MONTH(vDate),DAY(vDate) INTO vMonth,vDay;
+    IF (vMonth < 13 AND vMonth > 0) AND (vDay > 0 AND vDay < 32)
+    THEN return 1;
+    ELSE return 0;
+    END IF;
+END$$
+DELIMITER ;
